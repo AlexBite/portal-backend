8758ceda898a83887577111519101893af435e75
Alexander Bologov <sano.strike14110@gmail.com>
2023-05-05 00:08:34 +0500

    ef-core setup

---
+++ Core/Data/ApiDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Core.Data;
+
+public class ApiDbContext : DbContext
+{
+    public ApiDbContext(DbContextOptions<ApiDbContext> options) : base(options)
+    {
+    }
+}
--- Core/Program.cs
+++ Core/Program.cs
@@ -1,3 +1,6 @@
+using Core.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddEntityFrameworkNpgsql()
+    .AddDbContext<ApiDbContext>(opt =>
+        opt.UseNpgsql(builder.Configuration.GetConnectionString("DbConnection")));
 
 var app = builder.Build();
 
