106d0165deb4d568393ce06577598a02e5a779e6
Alexander Bologov <sano.strike14110@gmail.com>
2023-05-11 20:29:12 +0500

    Настройка CORS для локальной разработки

--- Core/Program.cs
+++ Core/Program.cs
@@ -10,18 +10,23 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<ApiDbContext>(opt =>
-        opt.UseNpgsql(builder.Configuration.GetConnectionString("DbConnection")));
+    opt.UseNpgsql(builder.Configuration.GetConnectionString("DbConnection")));
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+    app.UseCors(builder => builder
+        .AllowAnyHeader()
+        .AllowAnyMethod()
+        .AllowAnyOrigin());
+
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
+if (app.Environment.IsProduction()) app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
